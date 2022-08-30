67be5b9c2d2d437b0452037eaac0d7ad69e5841b
MitalParikh <111813615+MitalParikh@users.noreply.github.com>
2022-08-30 16:54:18 -0500

    changed page routing info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
-		{ path: 'about', component: AboutComponent },
-		{ path: 'projects', component: ProjectsComponent },
-		{ path: 'products', component: ProductsComponent },
-		{ path: 'gallery', component: GalleryComponent },
+		{ path: '/about', component: AboutComponent },
+		{ path: 'filigree/projects', component: ProjectsComponent },
+		{ path: 'filigree/products', component: ProductsComponent },
+		{ path: 'filigree/gallery', component: GalleryComponent },
 		{ path: '',   redirectTo: '/about', pathMatch: 'full' }, // redirect to `first-component`
 ];
 
