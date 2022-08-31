ce4f4590344d80b95ce624e3ddf12d0e2c5336e5
MitalParikh <111813615+MitalParikh@users.noreply.github.com>
2022-08-30 21:53:08 -0500

    Update app-routing.module.ts
    Co-Authored-By: MitalParikh <111813615+MitalParikh@users.noreply.github.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
-		{ path: '/about', component: AboutComponent },
-		{ path: '/projects', component: ProjectsComponent },
+		{ path: '/filigree/about', component: AboutComponent },
+		{ path: '/filigree/projects', component: ProjectsComponent },
 		{ path: '/products', component: ProductsComponent },
 		{ path: '/gallery', component: GalleryComponent },
 		{ path: '',   redirectTo: '/about', pathMatch: 'full' }, // redirect to `first-component`
