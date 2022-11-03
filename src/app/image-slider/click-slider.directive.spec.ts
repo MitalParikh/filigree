0583bb08ac5a42ef955f7ddcdc7e009785ea70e8
MitalParikh <111813615+MitalParikh@users.noreply.github.com>
2022-11-03 10:31:50 -0500

    updating project and product components

--- src/app/image-slider/click-slider.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ClickSliderDirective } from './click-slider.directive';
-
-describe('ClickSliderDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ClickSliderDirective();
-    expect(directive).toBeTruthy();
-  });
-});
